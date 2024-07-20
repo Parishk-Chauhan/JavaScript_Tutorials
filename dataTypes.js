ea03be2abe141e24ba77d4ec193c789a4e0342d1
Parishk Chauhan <parishkchauhan@gmail.com>
2024-07-20 13:13:49 +0530

    data types in js

---
+++ dataTypes.js
@@ -0,0 +1,31 @@
+/*
+types of data types in js : primitive and non-primitive 
+
+types of primitive data types in js : 
+
+1. number : the integers decimals fraction are in this 
+
+2. string : contnuous line of characters
+
+3. null : when there is not any value in the declare variable not like undefined when there we have not initialized with a value its is a standalone value it represent the intentional absence of a value 
+
+4. undefined : when we have not initialised any value to a variable
+
+5. Bigint : there is a range to number so bigger no. are in this 
+
+6. boolean : true and false values 
+
+7. symbol : A Symbol is a unique and immutable primitive value and may be used as the key of an Object property. The purpose of symbols is to create unique property keys that are guaranteed not to clash with keys from other code.
+
+Non-primitive : 
+
+1. objects : objects are the only mutable values. Functions are, in fact, also objects with the additional capability of being callable. (arrays and functions)
+*/
+
+let yearOfBirth = 2003   // number
+let montOfBirth = "december"   //string
+let isAlive = true   // boolean
+let hobby = null;   // null
+let interest;    // undefined
+
+// symbols will be studied in depth later. 
